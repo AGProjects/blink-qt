62066c588927886152bc32ca83a34f9965ba1ffe
Dan Pascu <dan@ag-projects.com>
2010-07-07 20:30:34 +0000

    Simplified about panel code

--- blink/aboutpanel.py
+++ blink/aboutpanel.py
@@ -6,7 +6,6 @@ from __future__ import with_statement
 __all__ = ['AboutPanel']
 
 from PyQt4 import uic
-from PyQt4.QtGui  import QFontMetrics
 
 from blink import __date__, __version__
 from blink.resources import Resources
@@ -48,7 +47,7 @@ class AboutPanel(base_class, ui_class):
 
         self.version.setText(u'Version %s\n%s' % (__version__, __date__))
 
-        credits_width = QFontMetrics(self.credits_text.currentFont()).width("NLNET Foundation" + "http://sipsimpleclient.com") + 40
+        credits_width = self.credits_text.fontMetrics().width("NLNET Foundation" + "http://sipsimpleclient.com") + 40
         self.credits_text.setFixedWidth(credits_width)
         self.credits_text.document().documentLayout().documentSizeChanged.connect(self._credits_size_changed)
         self.credits_text.setHtml(credits_text)
@@ -56,7 +55,7 @@ class AboutPanel(base_class, ui_class):
     def _credits_size_changed(self, size):
         self.credits_text.document().documentLayout().documentSizeChanged.disconnect(self._credits_size_changed)
         self.setFixedSize(self.minimumSize().width(), self.minimumSize().width()*1.40) # set a fixed aspect ratio
-        row_height = QFontMetrics(self.credits_text.currentFont()).height() + 2 # +2 for cellspacing
+        row_height = self.credits_text.fontMetrics().height() + 2 # +2 for cellspacing
         max_credits_height = 8*row_height + 2 + 14 # allow for maximum 8 rows; +2 for cellspacing and +14 for top/bottom margins
         if self.credits_text.height() > max_credits_height:
             self.setFixedHeight(self.height() - (self.credits_text.height() - max_credits_height))
