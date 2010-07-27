3bf1a7f52aff026d4f2d89b3b28a9d3bbdafcfcb
Saul Ibarra <saul@ag-projects.com>
2010-07-27 08:41:11 +0000

    Added MANIFEST.in, setup.py and Debian packaging files

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os
+import glob
+
+import blink
+
+
+def find_packages(toplevel):
+    return [directory.replace('/', '.') for directory, subdirs, files in os.walk(toplevel) if '__init__.py' in files]
+
+setup(name         = "blink",
+      version      = blink.__version__,
+      author       = "AG Projects",
+      author_email = "support@ag-projects.com",
+      url          = "http://icanblink.com",
+      description  = "Blink Qt",
+      long_description = "A state of the art, easy to use SIP client",
+      platforms    = ["Platform Independent"],
+      classifiers  = [
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: End Users/Desktop",
+          "License :: GNU General Public License 3 (GPLv3)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python"
+      ],
+      packages     = find_packages('blink'),
+      data_files   = [('bin', ['bin/blink']),
+                      ('share/blink', glob.glob('resources/*.ui')),
+                      ('share/blink/icons', glob.glob('resources/icons/*.png')+glob.glob('resources/icons/*.svg')),
+                      ('share/blink/sounds', glob.glob('resources/sounds/*.wav'))
+      ]
+)
+
