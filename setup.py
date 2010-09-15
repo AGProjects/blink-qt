e2de802d0098a239f15fece1ee05540acc654415
Dan Pascu <dan@ag-projects.com>
2010-09-15 18:08:31 +0000

    Removed build dependency on python-qt4

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@
 
 from distutils.core import setup
 import os
+import re
 import glob
 
-from blink import __version__
 
+def get_version():
+    return re.search(r"""__version__\s+=\s+(?P<quote>['"])(?P<version>.+?)(?P=quote)""", open('blink/__init__.py').read()).group('version')
 
 def find_packages(toplevel):
     return [directory.replace('/', '.') for directory, subdirs, files in os.walk(toplevel) if '__init__.py' in files]
 
 setup(name         = "blink",
-      version      = __version__,
+      version      = get_version(),
       author       = "AG Projects",
       author_email = "support@ag-projects.com",
       url          = "http://icanblink.com",
@@ -27,7 +29,7 @@ setup(name         = "blink",
       ],
       packages     = find_packages('blink'),
       data_files   = [('share/blink', glob.glob('resources/*.ui')),
-                      ('share/blink/icons', glob.glob('resources/icons/*.png')+glob.glob('resources/icons/*.svg')),
+                      ('share/blink/icons', glob.glob('resources/icons/*.png') + glob.glob('resources/icons/*.svg')),
                       ('share/blink/sounds', glob.glob('resources/sounds/*.wav'))
       ],
       scripts      = ['bin/blink', 'bin/blink-settings']
