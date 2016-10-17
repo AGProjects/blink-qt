ec85f0f3866774bd0fa6730d85893bd2acdf9505
Dan Pascu <dan@ag-projects.com>
2016-10-17 08:59:02 +0000

    Fixed style test viewers after Qt5 migration

--- resources/chat/styles/Stockholm/test/view.py
+++ resources/chat/styles/Stockholm/test/view.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
+import os
+
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebKit import QWebSettings
 from PyQt5.QtWebKitWidgets import QWebView
 from PyQt5.QtWidgets import QApplication
 
-app = QApplication([])
+app = QApplication(['mockup'])
 view = QWebView()
 settings = view.settings()
 settings.setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
-view.load(QUrl('mockup.html'))
+view.load(QUrl.fromLocalFile(os.path.realpath('mockup.html')))
 view.show()
 app.exec_()
