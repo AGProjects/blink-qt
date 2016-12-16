9829c9e04a858af99ed9318acbc11a591ecd0e29
Saul Ibarra <saul@ag-projects.com>
2016-12-16 15:31:55 +0000

    Raised version and updated changelog

--- blink/__info__.py
+++ blink/__info__.py
@@ -5,9 +5,9 @@ __project__ = "blink"
 __summary__ = "Blink Qt"
 __webpage__ = "http://icanblink.com/"
 
-__version__ = "2.0.0"
+__version__ = "3.0.0"
 
-__date__ = "March 9th 2016"
+__date__ = "December 16th 2016"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
