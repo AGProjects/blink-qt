bfd4e5e8fcb63e0b37dfe32b2438c9d77188ba10
Dan Pascu <dan@ag-projects.com>
2020-02-14 10:53:52 +0000

    Updated copyright years

--- blink/__info__.py
+++ blink/__info__.py
@@ -13,4 +13,4 @@ __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
 
 __license__ = "GPLv3"
-__copyright__ = "Copyright 2010-2019 {}".format(__author__)
+__copyright__ = "Copyright 2010-2020 {}".format(__author__)
