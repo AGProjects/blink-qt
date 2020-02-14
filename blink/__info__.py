3df82202527ebb976977626aa46049c937a47c4a
Dan Pascu <dan@ag-projects.com>
2020-02-14 11:09:28 +0000

    Increased version and updated changelog

--- blink/__info__.py
+++ blink/__info__.py
@@ -5,9 +5,9 @@ __project__ = "blink"
 __summary__ = "Blink Qt"
 __webpage__ = "http://icanblink.com/"
 
-__version__ = "3.2.0"
+__version__ = "3.2.1"
 
-__date__ = "February 27th 2019"
+__date__ = "February 14th 2020"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
