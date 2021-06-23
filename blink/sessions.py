9b2f4358034b5af95ec04fb52b7756892d41a1d5
Adrian Georgescu <ag@ag-projects.com>
2021-06-23 12:38:42 +0000

    Fixed auto_answer when using Bonjour

--- blink/sessions.py
+++ blink/sessions.py
@@ -5243,13 +5243,19 @@ class IncomingRequest(QObject):
         self.dialog.username_label.setText(contact.name or session.remote_identity.display_name or address)
         settings = SIPSimpleSettings()
             
-        if settings.audio.auto_answer and contact and contact.settings.auto_answer and settings.audio.auto_answer_interval:
-            self.dialog.setAutoAnswer(settings.audio.auto_answer_interval)
-            self._auto_answer_timer = QTimer()
-            self._auto_answer_timer.setInterval(settings.audio.auto_answer_interval * 1000)
-            self._auto_answer_timer.setSingleShot(True)
-            self._auto_answer_timer.timeout.connect(self._auto_answer)
-            self._auto_answer_timer.start()
+        try:
+            if settings.audio.auto_answer and contact.settings.auto_answer and settings.audio.auto_answer_interval:
+                auto_answer_interval = settings.audio.auto_answer_interval        
+        except AttributeError:
+            pass
+        else:
+            if auto_answer_interval > 0:
+                self.dialog.setAutoAnswer(settings.audio.auto_answer_interval)
+                self._auto_answer_timer = QTimer()
+                self._auto_answer_timer.setInterval(settings.audio.auto_answer_interval * 1000)
+                self._auto_answer_timer.setSingleShot(True)
+                self._auto_answer_timer.timeout.connect(self._auto_answer)
+                self._auto_answer_timer.start()
 
         self.dialog.user_icon.setPixmap(contact.icon.pixmap(48))
         self.dialog.audio_stream.setVisible(self.audio_stream is not None)
