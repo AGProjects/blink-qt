07c1b55b5c159d6998b9a7bddcaf43f55a9b5e36
Adrian Georgescu <ag@ag-projects.com>
2021-08-17 18:14:41 +0000

    Added version 5.1.4

--- blink/__info__.py
+++ blink/__info__.py
@@ -5,9 +5,9 @@ __project__ = "blink"
 __summary__ = "Blink Qt"
 __webpage__ = "https://icanblink.com/"
 
-__version__ = "5.1.3"
+__version__ = "5.1.4"
 
-__date__ = "July 7th, 2021"
+__date__ = "August 17th, 2021"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
