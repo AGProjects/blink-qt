cb06b58f6f0cf2906f56b6fb5460a2907bb34e85
Adrian Georgescu <ag@ag-projects.com>
2022-06-06 14:00:23 +0000

    Increase version

--- blink/__info__.py
+++ blink/__info__.py
@@ -5,12 +5,12 @@ __project__ = "blink"
 __summary__ = "Blink Qt"
 __webpage__ = "https://icanblink.com/"
 
-__version__ = "5.1.8"
+__version__ = "5.2.0"
 
-__date__ = "November 15th, 2021"
+__date__ = "June 6th, 2022"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
 
 __license__ = "GPLv3"
-__copyright__ = "Copyright 2010-2021 {}".format(__author__)
+__copyright__ = "Copyright 2010-2022 {}".format(__author__)
