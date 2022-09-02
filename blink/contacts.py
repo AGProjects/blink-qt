fb04cea9031148178ce29d82025446a5e1aa3571
Tijmen de Mes <tijmen@ag-projects.com>
2022-09-02 14:54:32 +0000

    Fixed starting messages to single uri

--- blink/contacts.py
+++ blink/contacts.py
@@ -3538,7 +3538,12 @@ class ContactListView(QListView):
     def _AH_SendSMS(self, uri=None):
         contact = self.selectionModel().selectedIndexes()[0].data(Qt.UserRole)
         session_manager = MessageManager()
-        session_manager.create_message_session(uri.uri or str(contact.uri.uri))
+        try:
+            uri = uri.uri
+        except AttributeError:
+            uri = uri
+
+        session_manager.create_message_session(uri or contact.uri.uri)
 
     def _AH_SendFiles(self, uri=None):
         session_manager = SessionManager()
