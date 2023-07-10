d0dfc935d41b057df779ecfe231b69a7e0731a9d
Tijmen de Mes <tijmen@ag-projects.com>
2023-07-10 12:23:53 +0000

    Increased version and updated changelog

--- blink/__info__.py
+++ blink/__info__.py
@@ -5,12 +5,12 @@ __project__ = "blink"
 __summary__ = "Blink Qt"
 __webpage__ = "https://icanblink.com/"
 
-__version__ = "5.5.1"
+__version__ = "5.6.0"
 
-__date__ = "November 6th, 2022"
+__date__ = "July 10th, 2023"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
 
 __license__ = "GPLv3"
-__copyright__ = "Copyright 2010-2022 {}".format(__author__)
+__copyright__ = "Copyright 2010-2023 {}".format(__author__)
