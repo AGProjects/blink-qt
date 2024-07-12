d6a764f5f260ed1827a23e16da8a603f96dc7cfe
Tijmen de Mes <tijmen@ag-projects.com>
2024-07-12 13:56:08 +0000

    Lower blink version number

--- blink/__info__.py
+++ blink/__info__.py
@@ -5,9 +5,9 @@ __project__ = "blink"
 __summary__ = "Blink Qt"
 __webpage__ = "https://icanblink.com/"
 
-__version__ = "6.0.0"
+__version__ = "5.9.0"
 
-__date__ = "June 21st, 2024"
+__date__ = "July 12st, 2024"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
