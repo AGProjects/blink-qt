bf41663f41d8659aef27c27f1b4f1cece2824f57
Adrian Georgescu <ag@ag-projects.com>
2024-07-25 13:29:40 +0000

    Load last conversations when opening Meessage window if no previous chat loaded

--- blink/chatwindow.py
+++ blink/chatwindow.py
@@ -2657,6 +2657,9 @@ class ChatWindow(base_class, ui_class, ColorHelperMixin):
         super(ChatWindow, self).show()
         self.raise_()
         self.activateWindow()
+        if not self.session_model.rowCount():
+            history = HistoryManager()
+            history.get_last_contacts()
 
     def show_session_info(self):
         self.show()
