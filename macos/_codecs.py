04ea7659f9f1164fa18631eea4b79aefb8b43214
Adrian Georgescu <ag@ag-projects.com>
2024-08-08 11:51:37 +0000

    Added G722 to default audio codecs on mac

--- macos/_codecs.py
+++ macos/_codecs.py
@@ -2,6 +2,6 @@ from sipsimple.configuration.datatypes import AudioCodecList, VideoCodecList
 from sipsimple.configuration.settings import RTPSettings, Setting
 
 class RTPSettingsExtension(RTPSettings):
-    audio_codec_order = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'PCMU', 'PCMA')))
+    audio_codec_order = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'G722', 'PCMU', 'PCMA')))
     video_codec_order = Setting(type=VideoCodecList, default=VideoCodecList(('H264', 'VP8', 'VP9')))
 
