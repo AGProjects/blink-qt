3aa3127d27b07a1b6ea6ff92966e5a8cf5ce8a53
Adrian Georgescu <ag@ag-projects.com>
2024-12-23 15:14:17 +0000

    Increased version and added changelog for 6.0.0

--- blink/__info__.py
+++ blink/__info__.py
@@ -5,9 +5,9 @@ __project__ = "blink"
 __summary__ = "Blink Qt"
 __webpage__ = "https://icanblink.com/"
 
-__version__ = "5.9.1"
+__version__ = "6.0.0"
 
-__date__ = "July 17th, 2024"
+__date__ = "December 23rd, 2024"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
