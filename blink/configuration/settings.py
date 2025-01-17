5a720088f98862205efe8c981ac8026730058a33
Adrian Georgescu <ag@ag-projects.com>
2025-01-17 16:56:48 +0000

    Enable TLS certificate verification by default

--- blink/configuration/settings.py
+++ blink/configuration/settings.py
@@ -84,7 +84,7 @@ except ImportError:
     class TLSSettingsExtension(TLSSettings):
         ca_list = Setting(type=ApplicationDataPath, default=ApplicationDataPath(Resources.get('tls/ca.crt')), nillable=True)
         certificate = Setting(type=ApplicationDataPath, default=ApplicationDataPath(Resources.get('tls/default.crt')), nillable=True)
-        verify_server = Setting(type=bool, default=False)
+        verify_server = Setting(type=bool, default=True)
 
 
 class SIPSimpleSettingsExtension(SettingsObjectExtension):
