6f0e409f5b1ad35c65c28f14b3d389400a13dfc3
Adrian Georgescu <ag@ag-projects.com>
2025-01-24 16:58:35 +0000

    Remove obsolete package in mac app build script

--- macos/make_spec.sh
+++ macos/make_spec.sh
@@ -4,6 +4,6 @@
 # the file must be tweaked, see the modified blink.specs file
 
 pyinstaller --add-data macos:macos --add-data resources:share/blink --add-data blink:blink \
---hidden-import=application --hidden-import=PyQt5.QtSvg \
---osx-bundle-identifier com.ag-projects.blink-qt --osx-entitlements-file macos/Blink.entitlements \
+--hidden-import=application \
+ --osx-bundle-identifier com.ag-projects.blink-qt --osx-entitlements-file macos/Blink.entitlements \
 --icon=macos/blink.icns -n 'Blink' --windowed --argv-emulation blink-run.py
