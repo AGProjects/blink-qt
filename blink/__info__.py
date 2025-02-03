57a74d31510e1fb0def74076d4439f3eb62315dc
Tijmen de Mes <tijmen@ag-projects.com>
2025-02-03 09:40:19 +0000

    Updated date

--- blink/__info__.py
+++ blink/__info__.py
@@ -7,7 +7,7 @@ __webpage__ = "https://icanblink.com/"
 
 __version__ = "6.0.2"
 
-__date__ = "January 31rd, 2025"
+__date__ = "February 3rd, 2025"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
