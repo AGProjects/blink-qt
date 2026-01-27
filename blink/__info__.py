f48affecd77efe3489cba8a887bab99a75f20c16
Tijmen de Mes <tijmen@ag-projects.com>
2026-01-27 12:48:49 +0000

    Updated copyright date

--- blink/__info__.py
+++ blink/__info__.py
@@ -13,4 +13,4 @@ __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
 
 __license__ = "GPLv3"
-__copyright__ = "Copyright 2010-2025 {}".format(__author__)
+__copyright__ = "Copyright 2010-2026 {}".format(__author__)
